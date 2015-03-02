c5908a167b291809c5f94139aff437d3387430c8
hiflyer <per.malmen@gmail.com>
2015-03-02 15:57:20 +0100

    Added some tests for the router

---
+++ src/test/java/se/fearless/service/RouterTest.java
@@ -0,0 +1,88 @@
+package se.fearless.service;
+
+import com.googlecode.gentyref.TypeToken;
+import io.netty.buffer.ByteBuf;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.reactivex.netty.protocol.http.server.HttpServerRequest;
+import io.reactivex.netty.protocol.http.server.HttpServerResponse;
+import io.reactivex.netty.protocol.http.server.RequestHandler;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.regex.Pattern;
+
+import static org.junit.Assert.*;
+import static se.mockachino.Mockachino.*;
+
+public class RouterTest {
+
+	private Router router;
+	private final static TypeToken<RequestHandler<ByteBuf, ByteBuf>> REQUEST_HANDLER_TYPE_TOKEN = new TypeToken<RequestHandler<ByteBuf, ByteBuf>>() {
+	};
+	private final static TypeToken<HttpServerRequest<ByteBuf>> REQUEST_TYPE_TOKEN = new TypeToken<HttpServerRequest<ByteBuf>>() {
+	};
+	private final static TypeToken<HttpServerResponse<ByteBuf>> RESPONSE_TYPE_TOKEN = new TypeToken<HttpServerResponse<ByteBuf>>() {
+	};
+	private RequestHandler<ByteBuf, ByteBuf> handler;
+	private HttpServerRequest<ByteBuf> request;
+	private HttpServerResponse<ByteBuf> response;
+
+
+	@Before
+	public void setUp() throws Exception {
+		router = new Router();
+		handler = mock(REQUEST_HANDLER_TYPE_TOKEN);
+		request = mock(REQUEST_TYPE_TOKEN);
+		response = mock(RESPONSE_TYPE_TOKEN);
+	}
+
+	@Test
+	public void whenCalledWithExactMatchStaticRoutingInvokesHandler() throws Exception {
+		setupGetForPath("/foo");
+
+		router.addRoute(HttpMethod.GET, "/foo", handler);
+		router.route(request, response);
+
+		verifyOnce().on(handler).handle(request, response);
+	}
+
+	@Test
+	public void whenCalledWithMismatchStaticRoutingReturns404() throws Exception {
+		setupGetForPath("/bar");
+
+		router.addRoute(HttpMethod.GET, "/foo", handler);
+		router.route(request, response);
+
+		verifyFail();
+	}
+
+	@Test
+	public void whenCalledWithMatchingExpressionRoutingInvokesHandler() throws Exception {
+		setupGetForPath("/match23");
+
+		router.addRoute(HttpMethod.GET, Pattern.compile("/match[\\d]+"), handler);
+		router.route(request, response);
+
+		verifyOnce().on(handler).handle(request, response);
+	}
+
+	@Test
+	public void whenCalledWithNonMatchingExpressionRoutingReturns404() throws Exception {
+		setupGetForPath("/matching");
+
+		router.addRoute(HttpMethod.GET, Pattern.compile("/match[\\d]+"), handler);
+		router.route(request, response);
+
+		verifyFail();
+	}
+
+	private void setupGetForPath(String value) {
+		when(request.getHttpMethod()).thenReturn(io.netty.handler.codec.http.HttpMethod.GET);
+		when(request.getPath()).thenReturn(value);
+	}
+
+	private void verifyFail() {
+		verifyNever().on(handler).handle(request, response);
+		verifyOnce().on(response).setStatus(HttpResponseStatus.NOT_FOUND);
+	}
+}
